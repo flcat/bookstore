161b0e47449a1c37f6d3f9da166c62e87fecbb86
flcat <flcat.xd@gmail.com>
2022-03-31 23:17:04 +0900

    structural change

--- src/main/java/shop/bookstore/HomeController.java
+++ src/main/java/shop/bookstore/HomeController.java
@@ -1,4 +1,4 @@
-package shop.bookstore.order;
+package shop.bookstore;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
--- src/main/java/shop/bookstore/order/OrderController.java
+++ src/main/java/shop/bookstore/order/OrderController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class orderController {
+public class OrderController {
 
     private final OrderService orderService;
     private final MemberService memberService;
